a6a5de333edfe2d502d0c73a2402549ed5b5bc98
Avery Rozar <avery.rozar@trolleyesecurity.com>
2024-08-10 09:18:43 -0400

    change from bucket to cloudwatch

--- config/structs.go
+++ config/structs.go
@@ -1,10 +1,10 @@
 package config
 
 type Config struct {
-	SentryIoDsn        string `json:"sentry_io_dsn"`
-	AwsRegion          string `json:"aws_region"`
-	AwsAccessKeyId     string `json:"aws_access_key_id"`
-	AwsSecretAccessKey string `json:"aws_secret_access_key"`
-	BucketName         string `json:"bucket_name"`
-	ListenPort         int    `json:"listen_port"`
+	SentryIoDsn         string `json:"sentry_io_dsn"`
+	AwsRegion           string `json:"aws_region"`
+	AwsAccessKeyId      string `json:"aws_access_key_id"`
+	AwsSecretAccessKey  string `json:"aws_secret_access_key"`
+	CloudWatchGroupName string `json:"cloud_watch_group_name"`
+	ListenPort          int    `json:"listen_port"`
 }
--- syslogs/structs.go
+++ syslogs/structs.go
@@ -1,5 +1,7 @@
 package syslogs
 
+import "time"
+
 type Log struct {
 	SrcAddr string `json:"srcAddr"`
 	Hdr     string `json:"hdr"`
@@ -14,13 +16,21 @@ type Task struct {
 }
 
 type CefMessage struct {
-	CefVersion     int    `json:"cef_version"`
-	ProductVendor  string `json:"product_vendor"`
-	Product        string `json:"product"`
-	ProductVersion string `json:"product_version"`
-	EventClass     string `json:"event_class"`
-	EventName      string `json:"event_name"`
-	EventSeverity  string `json:"event_severity"`
-	SyslogMsg      string `json:"syslog_msg"`
-	JsonMsg        string `json:"json_msg"`
+	RemoteAddr     string    `json:"remote_addr"`
+	CefVersion     int       `json:"cef_version"`
+	ProductVendor  string    `json:"product_vendor"`
+	Product        string    `json:"product"`
+	ProductVersion string    `json:"product_version"`
+	EventClass     string    `json:"event_class"`
+	EventName      string    `json:"event_name"`
+	EventSeverity  string    `json:"event_severity"`
+	SyslogMsg      string    `json:"syslog_msg"`
+	JsonMsg        string    `json:"json_msg"`
+	Time           time.Time `json:"time"`
+}
+
+type LogEvent struct {
+	Timestamp     int64  `bson:"timestamp" json:"timestamp"`
+	Message       string `bson:"message" json:"message"`
+	IngestionTime int64  `bson:"ingestion_time" json:"'ingestion_time"`
 }
