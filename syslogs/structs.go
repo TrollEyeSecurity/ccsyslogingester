c0170d5a160180b76fde87f33e820346e50cd424
Avery Rozar <avery.rozar@trolleyesecurity.com>
2024-08-10 14:09:00 -0400

    update log format

--- syslogs/structs.go
+++ syslogs/structs.go
@@ -2,19 +2,6 @@ package syslogs
 
 import "time"
 
-type Log struct {
-	SrcAddr string `json:"srcAddr"`
-	Hdr     string `json:"hdr"`
-	Msg     string `json:"msg"`
-}
-
-type Task struct {
-	Id     string `json:"id"`
-	LogId  string `json:"log_id"`
-	Type   string `json:"type"`
-	Source string `json:"source"`
-}
-
 type CefMessage struct {
 	RemoteAddr     string    `json:"remote_addr"`
 	CefVersion     int       `json:"cef_version"`
@@ -25,12 +12,6 @@ type CefMessage struct {
 	EventName      string    `json:"event_name"`
 	EventSeverity  string    `json:"event_severity"`
 	SyslogMsg      string    `json:"syslog_msg"`
-	JsonMsg        string    `json:"json_msg"`
+	JsonMsg        []byte    `json:"json_msg"`
 	Time           time.Time `json:"time"`
-}
-
-type LogEvent struct {
-	Timestamp     int64  `bson:"timestamp" json:"timestamp"`
-	Message       string `bson:"message" json:"message"`
-	IngestionTime int64  `bson:"ingestion_time" json:"'ingestion_time"`
 }
